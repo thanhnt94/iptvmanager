33dac12c54ea6e5739deb1c3ebbd753d6fbd3473
Thanh Nguyen <thanhnt94@gmail.com>
2026-06-28 18:52:56 +0700

    Auto commit from remote update tool - 2026-06-28 18:52:56

--- iptv-studio/vite.config.ts
+++ iptv-studio/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 11']
+    })
+  ],
   base: '/',
   build: {
     outDir: '../app/static/dist',
